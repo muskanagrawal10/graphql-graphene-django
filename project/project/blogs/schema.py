5455960083eae993bf41210c75b5dc92399f6057
Muskan Agrawal <agrawal.9@iitj.ac.in>
2021-12-31 20:01:46 +0530

    Second Commit

--- project/project/blogs/schema.py
+++ project/project/blogs/schema.py
@@ -22,3 +22,52 @@ class Query(graphene.ObjectType):
         return Blog.objects.all()
 
 schema= graphene.Schema(query = Query)
+
+class BlogInput(graphene.InputObjectType):
+    title = graphene.String()
+    author = graphene.String()
+    body = graphene.String()
+    likes = graphene.Int()
+    dislikes = graphene.Int()
+    
+class CreateBlog(graphene.Mutation):
+    class Arguments:
+        input = BlogInput(required=True)
+
+    blog = graphene.Field(BlogType)
+    
+    @classmethod
+    def mutate(cls, root, info, input):
+        blog = Blog()
+        blog.title = input.title
+        blog.author = input.author
+        blog.body = input.body
+        blog.likes = input.likes
+        blog.dislikes = input.dislikes
+        blog.save()
+        return CreateBlog(blog=blog)
+
+class UpdateBlog(graphene.Mutation):
+    class Arguments:
+        input = BlogInput(required=True)
+        id = graphene.ID()
+
+    blog = graphene.Field(BlogType)
+    
+    @classmethod
+    def mutate(cls, root, info, input, id):
+        blog = Blog.objects.get(pk=id)
+        # blog.title = input.title
+        blog.author = input.author
+        blog.body = input.body
+        # blog.likes = input.likes
+        # blog.dislikes = input.dislikes
+        blog.save()
+        return UpdateBlog(blog=blog)
+
+class Mutation(graphene.ObjectType):
+
+    create_blog = CreateBlog.Field()
+    update_blog = UpdateBlog.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
